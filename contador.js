e868fbd8bbe3b323e59d26d780a0f007b2e28e8b
Luis Serafim <fernandodss30@hotmail.com>
2021-07-09 09:35:37 -0300

    Botão adicionado

--- contador.js
+++ contador.js
@@ -1,29 +1,34 @@
 window.addEventListener("load", () => {
-    //Tempo, em segundos.
-    let sec = 360;
-
-    const countDiv = document.getElementById("timer");
     
-    const secpass = () => {
-        let min = Math.floor(sec / 60);
-        let segundosRestantes = sec % 60;
+    const botao = document.getElementById("botao");
 
-        if (segundosRestantes < 10) {
-            segundosRestantes = "0" + segundosRestantes;
-        }
-        if (min < 10) {
-            min = "0" + min;
-        }
-        //Gera o formato 02:05
-        countDiv.innerHTML = min + ":" + segundosRestantes;
-        //Condição final
-        if (sec > 0) {
-            sec = sec - 1;
-        }
-        else {
-            countDiv.innerHTML = "Acabou!";
-        }
-    }
+    botao.addEventListener("click", () => {
+        //Tempo, em segundos.
+        let sec = 360;
 
-    const countDown = setInterval(() => secpass(), 1000);
+        const countDiv = document.getElementById("timer");
+
+        const secpass = () => {
+            let min = Math.floor(sec / 60);
+            let segundosRestantes = sec % 60;
+
+            if (segundosRestantes < 10) {
+                segundosRestantes = "0" + segundosRestantes;
+            }
+            if (min < 10) {
+                min = "0" + min;
+            }
+            //Gera o formato 02:05
+            countDiv.innerHTML = min + ":" + segundosRestantes;
+            //Condição final
+            if (sec > 0) {
+                sec = sec - 1;
+            }
+            else {
+                countDiv.innerHTML = "Acabou!";
+            }
+        }
+
+        const countDown = setInterval(() => secpass(), 1000);
+    });
 });
