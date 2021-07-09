4499f58f8b077db811cf142860b7fd3d451438d0
Luis Serafim <fernandodss30@hotmail.com>
2021-07-09 09:25:02 -0300

    update contador.js
    Contador funcionando.

---
+++ contador.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", () => {
+    //Tempo, em segundos.
+    let sec = 180;
+
+    const countDiv = document.getElementById("timer");
+    
+    const secpass = () => {
+        let min = Math.floor(sec / 60);
+        let segundosRestantes = sec % 60;
+
+        if (segundosRestantes < 10) {
+            segundosRestantes = "0" + segundosRestantes;
+        }
+        if (min < 10) {
+            min = "0" + min;
+        }
+        //Gera o formato 02:05
+        countDiv.innerHTML = min + ":" + segundosRestantes;
+        //Condição final
+        if (sec > 0) {
+            sec = sec - 1;
+        }
+        else {
+            countDiv.innerHTML = "Acabou!";
+        }
+    }
+
+    const countDown = setInterval(() => secpass(), 1000);
+});
